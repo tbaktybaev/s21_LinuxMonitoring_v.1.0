de50838e263315394493063a12e748bc17cc77ea
Тынарбек Бактыбаев <tbaktybaev@MacBook-Pro-2.local>
2023-12-17 00:10:45 +0300

    1 and 2 tasks done

--- src/01/main.sh
+++ src/01/main.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-usage() {
-    echo "Usage : $0 <text_parameter>"
+if [ "$#" -ne 1 ]; then 
+    echo "Usage: $0 <text_parameter>"
     exit 1
-}
+fi
+
+source functions.sh
+
+check_and_display "$1"
