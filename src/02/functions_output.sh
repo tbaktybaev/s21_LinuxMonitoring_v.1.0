450affefc03c1e7f9ec6b8164f11b91beffd830e
Тынарбек Бактыбаев <tbaktybaev@MacBook-Pro-2.local>
2023-12-17 00:43:41 +0300

    1 and 2 tasks done

--- src/02/functions_output.sh
+++ src/02/functions_output.sh
@@ -23,22 +23,23 @@ save_to_file() {
     read -p "Хотите сохранить данные в файл? (Y/N): " answer
     if [ "$answer" == "Y" ] || [ "$answer" == "y" ]; then
         filename=$(date +"%d_%m_%y_%H_%M_%S.status")
-        echo -e "HOSTNAME = $HOSTNAME\n
-                 TIMEZONE = $TIMEZONE\n
-                 USER = $USER\n
-                 OS = $OS\n
-                 DATE = $DATE\n
-                 UPTIME = $UPTIME\n
-                 UPTIME_SEC = $UPTIME_SEC\n
-                 IP = $IP\n
-                 MASK = $MASK\n
-                 GATEWAY = $GATEWAY\n
-                 RAM_TOTAL = $RAM_TOTAL\n
-                 RAM_USED = $RAM_USED\n
-                 RAM_FREE = $RAM_FREE\n
-                 SPACE_ROOT = $SPACE_ROOT\n
-                 SPACE_ROOT_USED = $SPACE_ROOT_USED\n
-                 SPACE_ROOT_FREE = $SPACE_ROOT_FREE" > "$filename"
+        # echo -e "HOSTNAME = $HOSTNAME\n
+        #          TIMEZONE = $TIMEZONE\n
+        #          USER = $USER\n
+        #          OS = $OS\n
+        #          DATE = $DATE\n
+        #          UPTIME = $UPTIME\n
+        #          UPTIME_SEC = $UPTIME_SEC\n
+        #          IP = $IP\n
+        #          MASK = $MASK\n
+        #          GATEWAY = $GATEWAY\n
+        #          RAM_TOTAL = $RAM_TOTAL\n
+        #          RAM_USED = $RAM_USED\n
+        #          RAM_FREE = $RAM_FREE\n
+        #          SPACE_ROOT = $SPACE_ROOT\n
+        #          SPACE_ROOT_USED = $SPACE_ROOT_USED\n
+        #          SPACE_ROOT_FREE = $SPACE_ROOT_FREE" > "$filename"
+        display_info > "$filename"
         echo "Данные сохранены в файл: $filename"
     fi
 }
