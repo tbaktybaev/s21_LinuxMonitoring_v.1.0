faa7fcf69f50e54d26d50d2acb7fb411ad7f7f88
Santo Baba <santobab@student.21-school.ru>
2023-12-17 22:55:49 +0300

    commit

--- src/04/main.sh
+++ src/04/main.sh
@@ -26,29 +26,15 @@ font_hostname=$(map_color "${column1_font_color:-$default_column1_font_color}")
 bg_values=$(map_color "${column2_background:-$default_column2_background}")
 font_values=$(map_color "${column2_font_color:-$default_column2_font_color}")
 
-bg_1=${column1_background:-default_column1_background}
-ft_1=${column1_font_color:-default_column1_font_color}
-bg_2=${column2_background:-default_column2_background}
-ft_2=${column2_font_color:-default_column2_font_color}
+bg_1=${column1_background:-$default_column1_background}
+ft_1=${column1_font_color:-$default_column1_font_color}
+bg_2=${column2_background:-$default_column2_background}
+ft_2=${column2_font_color:-$default_column2_font_color}
 
 bg_1_def=${column1_background:-default}
 ft_1_def=${column1_font_color:-default}
 bg_2_def=${column2_background:-default}
 ft_2_def=${column2_font_color:-default}
-
-echo $bg_1
-echo $ft_1
-echo $bg_2
-echo $ft_2
-
-# Вывод цветовой схемы
-display_color_scheme() {
-    echo -e "\nColor Scheme:"
-    echo "Column 1 background = $bg_1_def ($(map_color_scheme "$bg_1"))"
-    echo "Column 1 font color = $ft_1_def ($(map_color_scheme "$ft_1"))"
-    echo "Column 2 background = $bg_2_def ($(map_color_scheme "$bg_2"))"
-    echo "Column 2 font color = $ft_2_def ($(map_color_scheme "$ft_2"))"
-}
 
 # Вызов функции для получения системной информации
 get_system_info
