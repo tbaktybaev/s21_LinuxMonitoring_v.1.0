0dfbc76d56b52f4c425a19ad21555b259f47a72a
Santo Baba <santobab@student.21-school.ru>
2023-12-17 22:43:23 +0300

    commit

--- src/04/functions_output.sh
+++ src/04/functions_output.sh
@@ -2,7 +2,7 @@
 display_color_scheme() {
     echo -e "\nColor Scheme:"
     echo "Column 1 background = $bg_1 ($(map_color_scheme "$bg_1"))"
-    echo "Column 1 font color = $ft_1 ($(map_color_scheme "$font_hostname"))"
+    echo "Column 1 font color = $ft_1 ($(map_color_scheme "$ft_1"))"
     echo "Column 2 background = $bg_2 ($(map_color_scheme "$bg_2"))"
     echo "Column 2 font color = $ft_2 ($(map_color_scheme "$ft_2"))"
 }
