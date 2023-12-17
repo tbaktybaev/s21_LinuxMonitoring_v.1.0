72f184857db0e355308b0969da5c4c8190c6a4ee
Santo Baba <santobab@student.21-school.ru>
2023-12-17 21:22:40 +0300

    commit

--- src/03/main.sh
+++ src/03/main.sh
@@ -3,24 +3,7 @@ source ../02/functions_system.sh
 source functions_output.sh
 source functions_transform.sh
 
-# Проверка наличия 4 параметров
-if [ "$#" -ne 4 ]; then
-    echo "Ошибка: Необходимо ввести 4 параметра."
-    exit 1
-fi
-
-# Чтение параметров
-bg_hostname=$(map_color "$1")
-font_hostname=$(map_color "$2")
-bg_values=$(map_color "$3")
-font_values=$(map_color "$4")
-
-# Проверка на совпадение цветов
-if [ "$1" -eq "$2" ] || [ "$3" -eq "$4" ]; then
-    echo "Ошибка: Цвета фона и шрифта для одного столбца не должны совпадать."
-    exit 1
-fi
-
 # Вызов функции для получения системной информации
+read_and_check_colors "$1" "$2" "$3" "$4"
 get_system_info
 display_info_colored
