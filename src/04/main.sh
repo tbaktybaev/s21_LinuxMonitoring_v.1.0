5885636044e883066637167428e3e88514288ec9
Santo Baba <santobab@student.21-school.ru>
2023-12-17 22:16:06 +0300

    commit

--- src/04/main.sh
+++ src/04/main.sh
@@ -31,6 +31,6 @@ display_info_colored
 
 # Вывод цветовой схемы
 echo -e "\nColumn 1 background = $bg_hostname ($(map_color_scheme "$bg_hostname"))"
-echo "Column 1 font color = $font_hostname $(map_color_scheme "$font_hostname")"
-echo "Column 2 background = $bg_values $(map_color_scheme "$bg_values")"
-echo "Column 2 font color = $font_values $(map_color_scheme "$font_values")"
+echo "Column 1 font color = $font_hostname ($(map_color_scheme "$font_hostname"))"
+echo "Column 2 background = $bg_values ($(map_color_scheme "$bg_values"))"
+echo "Column 2 font color = $font_values ($(map_color_scheme "$font_values"))"
