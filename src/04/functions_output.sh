1180a348cecde733bdd16e8f4f81f1dfb5d3943a
Santo Baba <santobab@student.21-school.ru>
2023-12-17 23:00:01 +0300

    commit

--- src/04/functions_output.sh
+++ src/04/functions_output.sh
@@ -1,4 +1,22 @@
 # Вывод цветовой схемы
+check_and_set_variables() {
+    # Проверка заданности параметров и использование значений по умолчанию при необходимости
+    bg_hostname=$(map_color "${column1_background:-$default_column1_background}")
+    font_hostname=$(map_color "${column1_font_color:-$default_column1_font_color}")
+    bg_values=$(map_color "${column2_background:-$default_column2_background}")
+    font_values=$(map_color "${column2_font_color:-$default_column2_font_color}")
+
+    bg_1=${column1_background:-$default_column1_background}
+    ft_1=${column1_font_color:-$default_column1_font_color}
+    bg_2=${column2_background:-$default_column2_background}
+    ft_2=${column2_font_color:-$default_column2_font_color}
+
+    bg_1_def=${column1_background:-default}
+    ft_1_def=${column1_font_color:-default}
+    bg_2_def=${column2_background:-default}
+    ft_2_def=${column2_font_color:-default}
+}
+
 display_color_scheme() {
     echo -e "\nColor Scheme:"
     echo "Column 1 background = $bg_1_def ($(map_color_scheme "$bg_1"))"
--- src/04/main.sh
+++ src/04/main.sh
@@ -17,23 +17,6 @@ fi
 source "$config_file"
 source "$default"
 
-check_and_set_variables() {
-    # Проверка заданности параметров и использование значений по умолчанию при необходимости
-    bg_hostname=$(map_color "${column1_background:-$default_column1_background}")
-    font_hostname=$(map_color "${column1_font_color:-$default_column1_font_color}")
-    bg_values=$(map_color "${column2_background:-$default_column2_background}")
-    font_values=$(map_color "${column2_font_color:-$default_column2_font_color}")
-
-    bg_1=${column1_background:-$default_column1_background}
-    ft_1=${column1_font_color:-$default_column1_font_color}
-    bg_2=${column2_background:-$default_column2_background}
-    ft_2=${column2_font_color:-$default_column2_font_color}
-
-    bg_1_def=${column1_background:-default}
-    ft_1_def=${column1_font_color:-default}
-    bg_2_def=${column2_background:-default}
-    ft_2_def=${column2_font_color:-default}
-}
 # Вызов функции для получения системной информации
 check_and_set_variables
 get_system_info
