6254b415fbd79a3521513cce8202afc1d31bd09e
Santo Baba <santobab@student.21-school.ru>
2023-12-17 20:46:09 +0300

    commit

--- src/03/main.sh
+++ src/03/main.sh
@@ -8,11 +8,23 @@ if [ "$#" -ne 4 ]; then
     exit 1
 fi
 
+# Маппинг цветов
+map_color() {
+    case "$1" in
+        1) echo 15 ;;  # белый
+        2) echo 1 ;;   # красный
+        3) echo 2 ;;   # зеленый
+        4) echo 4 ;;   # синий
+        5) echo 0 ;;   # черный
+        *) echo "Ошибка: Недопустимый цвет." ;;
+    esac
+}
+
 # Чтение параметров
-bg_hostname=$1
-font_hostname=$2
-bg_values=$3
-font_values=$4
+bg_hostname=$(map_color "$1")
+font_hostname=$(map_color "$2")
+bg_values=$(map_color "$3")
+font_values=$(map_color "$4")
 
 # Проверка на совпадение цветов
 if [ "$1" -eq "$2" ] || [ "$3" -eq "$4" ]; then
