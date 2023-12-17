3c40341c344394f12fdaf6a7d2164c74a2b30f3f
Santo Baba <santobab@student.21-school.ru>
2023-12-17 22:54:37 +0300

    commit

--- src/04/functions_output.sh
+++
@@ -1,8 +0,0 @@
-# Вывод цветовой схемы
-display_color_scheme() {
-    echo -e "\nColor Scheme:"
-    echo "Column 1 background = $bg_1_def ($(map_color_scheme "$bg_1"))"
-    echo "Column 1 font color = $ft_1_def ($(map_color_scheme "$ft_1"))"
-    echo "Column 2 background = $bg_2_def ($(map_color_scheme "$bg_2"))"
-    echo "Column 2 font color = $ft_2_def ($(map_color_scheme "$ft_2"))"
-}
--- src/04/main.sh
+++ src/04/main.sh
@@ -31,10 +31,24 @@ ft_1=${column1_font_color:-default_column1_font_color}
 bg_2=${column2_background:-default_column2_background}
 ft_2=${column2_font_color:-default_column2_font_color}
 
-bg_1_def=${column1_background:default}
-ft_1_def=${column1_font_color:default}
-bg_2_def=${column2_background:default}
-ft_2_def=${column2_font_color:default}
+bg_1_def=${column1_background:-default}
+ft_1_def=${column1_font_color:-default}
+bg_2_def=${column2_background:-default}
+ft_2_def=${column2_font_color:-default}
+
+echo $bg_1_def
+echo $ft_1_def
+echo $bg_2_def
+echo $ft_2_def
+
+# Вывод цветовой схемы
+display_color_scheme() {
+    echo -e "\nColor Scheme:"
+    echo "Column 1 background = $bg_1_def ($(map_color_scheme "$bg_1"))"
+    echo "Column 1 font color = $ft_1_def ($(map_color_scheme "$ft_1"))"
+    echo "Column 2 background = $bg_2_def ($(map_color_scheme "$bg_2"))"
+    echo "Column 2 font color = $ft_2_def ($(map_color_scheme "$ft_2"))"
+}
 
 # Вызов функции для получения системной информации
 get_system_info
