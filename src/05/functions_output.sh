317e1c7807ac8ee07a304eb54b8f7b7e4cd15a76
Santo Baba <santobab@student.21-school.ru>
2023-12-18 19:28:13 +0300

    commit

--- src/05/functions_output.sh
+++ src/05/functions_output.sh
@@ -1,39 +1,45 @@
 #!/bin/bash
 
-# Начало отсчета времени выполнения скрипта
-start_time=$(date +%s.%N)
+start_time=$(date +%s)
 
-# Общее число папок (включая вложенные)
-total_folders=$(find "$directory" -type d | wc -l)
+dir_path="$1"
 
-# Топ 5 папок с самым большим весом
-top_folders=$(du -h --max-depth=1 "$directory" | sort -rh | head -n 6)
+get_size() {
+    du -sh "$1" | cut -f1
+}
 
-# Общее число файлов
-total_files=$(find "$directory" -type f | wc -l)
+get_md5() {
+    md5sum "$1" | cut -d' ' -f1
+}
 
-# Число файлов по типам
-config_files=$(find "$directory" -type f -name "*.conf" | wc -l)
-text_files=$(find "$directory" -type f -exec file {} \; | grep -c "text")
-executable_files=$(find "$directory" -type f -exec file {} \; | grep -c "executable")
-log_files=$(find "$directory" -type f -name "*.log" | wc -l)
-archive_files=$(find "$directory" -type f -exec file {} \; | grep -c "archive")
-symbolic_links=$(find "$directory" -type l | wc -l)
+total_folders=$(find "$dir_path" -type d | wc -l)
 
-# Топ 10 файлов с самым большим весом
-top_files=$(du -h "$directory"/* | sort -rh | head -n 11)
+top_folders=$(find "$dir_path" -type d -printf '%s %p\n' | sort -nr | head -n 5)
 
-# Топ 10 исполняемых файлов с хешем
-top_executables=$(find "$directory" -type f -exec file {} \; | grep "executable" | sort -k5 -rh | head -n 11)
+total_files=$(find "$dir_path" -type f | wc -l)
 
-# Рассчитываем время выполнения скрипта
-end_time=$(date +%s.%N)
-execution_time=$(echo "$end_time - $start_time" | bc)
 
-# Выводим результаты
+config_files=$(find "$dir_path" -type f -name "*.conf" | wc -l)
+text_files=$(find "$dir_path" -type f -exec file {} \; | grep "text" | wc -l)
+executable_files=$(find "$dir_path" -type f -executable | wc -l)
+log_files=$(find "$dir_path" -type f -name "*.log" | wc -l)
+archive_files=$(find "$dir_path" -type f -name "*.zip" -o -name "*.tar" -o -name "*.gz" | wc -l)
+symbolic_links=$(find "$dir_path" -type l | wc -l)
+
+top_files=$(find "$dir_path" -type f -printf '%s %p\n' | sort -nr | head -n 10)
+top_files_with_type=$(find "$dir_path" -type f -exec file {} \; | grep -v "directory" | sort -k2 | head -n 10)
+
+top_executables=$(find "$dir_path" -type f -executable -printf '%s %p\n' | sort -nr | head -n 10)
+top_executables_with_md5=$(find "$dir_path" -type f -executable -exec bash -c 'get_md5 "$0" && echo " $0"' {} \; | sort -nr | head -n 10)
+
+
+sleep 1
+end_time=$(date +%s)
+execution_time=$(echo "$end_time - $start_time" | bc -l)
+
 echo "Total number of folders (including all nested ones) = $total_folders"
 echo "TOP 5 folders of maximum size arranged in descending order (path and size):"
-echo "$top_folders" | awk '{printf "%d - %s, %s\n", NR, $2, $1}'
+echo "$top_folders" | awk '{print NR, "-", $2, $1}'
 echo "Total number of files = $total_files"
 echo "Number of:"
 echo "Configuration files (with the .conf extension) = $config_files"
@@ -43,12 +49,8 @@ echo "Log files (with the extension .log) = $log_files"
 echo "Archive files = $archive_files"
 echo "Symbolic links = $symbolic_links"
 echo "TOP 10 files of maximum size arranged in descending order (path, size and type):"
-echo "$top_files" | awk '{printf "%d - %s, %s, %s\n", NR, $2, $1, $NF}'
-echo "TOP 10 executable files of the maximum size arranged in descending order (path, size and MD5 hash of file)"
-while IFS= read -r line; do
-    path=$(echo "$line" | awk '{print $1}')
-    size=$(echo "$line" | awk '{print $3}')
-    hash=$(md5sum "$path" | awk '{print $1}')
-    printf "%s - %s, %s, %s\n" "$(basename "$path")" "$size" "$hash"
-done <<< "$top_executables"
+echo "$top_files_with_type" | awk '{print NR, "-", $2, $1, $3}'
+echo "TOP 10 executable files of the maximum size arranged in descending order (path, size and MD5 hash of file):"
+echo "$top_executables_with_md5" | awk '{print NR, "-", $2, $1, $3}'
 echo "Script execution time (in seconds) = $execution_time"
+
